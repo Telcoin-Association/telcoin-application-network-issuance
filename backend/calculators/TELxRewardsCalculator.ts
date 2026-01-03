4c9bf4c29a9d8c6d5905d72ff90e8e2a28a7ed9c
robriks <marsterlund@gmail.com>
2026-01-03 11:02:22 +0700

    publish period 20 rewards

--- backend/calculators/TELxRewardsCalculator.ts
+++ backend/calculators/TELxRewardsCalculator.ts
@@ -167,7 +167,7 @@ export const POOLS = [BASE_ETH_TEL, POLYGON_ETH_TEL, POLYGON_USDC_EMXN];
  * @notice While public-facing periods are 1-indexed, this utility uses `period 0` internally
  * to refer to the initialization period from pool creation to first period start.
  */
-export const PERIODS = Array.from({ length: 20 }, (_, i) => i);
+export const PERIODS = Array.from({ length: 21 }, (_, i) => i);
 const NETWORKS = {
   [ChainId.Polygon]: {
     poolManager: getAddress("0x67366782805870060151383f4bbff9dab53e5cd6"),
@@ -195,6 +195,7 @@ const NETWORKS = {
       80_109_849n, // dec 10
       80_408_956n, // dec 17
       80_729_178n, // dec 24
+      81_009_978n, // dec 31
     ],
   },
   [ChainId.Base]: {
@@ -223,6 +224,7 @@ const NETWORKS = {
       39_267_726n, // dec 10
       39_570_126n, // dec 17
       39_894_126n, // dec 24
+      40_174_926n, // dec 31
     ],
   },
 };
